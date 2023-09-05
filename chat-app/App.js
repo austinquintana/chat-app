5be9bcabdc3c0213b4310fd57c12310828e451d6
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-09-04 19:26:26 -0600

    Added NetInfo

--- chat-app/App.js
+++ chat-app/App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
+import { useEffect } from "react";
+import { Alert } from "react-native";
 
 import { LogBox } from "react-native";
 // Create the navigator
@@ -11,9 +13,13 @@ const Stack = createNativeStackNavigator();
 LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
+
+//importing useNetInfo for keeping track of the network's connectivity and updating in real time
+import { useNetInfo } from "@react-native-community/netinfo";
 
 const App = () => {
+  const connectionStatus = useNetInfo();
   const firebaseConfig = {
     apiKey: "AIzaSyDNWYp0Lf0A32eyfoBsJ-0_d-bH_nPDlig",
     authDomain: "chat-app-6ce28.firebaseapp.com",
@@ -24,6 +30,17 @@ const App = () => {
   };
   // initialize Firebase
   const app = initializeApp(firebaseConfig);
+
+  // throwing an error if no internet
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert("No internet connection")
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
+
   // initialize Cloud Firestore and get a reference to the service
   const db = getFirestore(app);
 
@@ -32,7 +49,7 @@ const App = () => {
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Chat">
-          {(props) => <Chat db={db} {...props} />}
+        {props => <Chat isConnected={connectionStatus.isConnected} db={db} {...props} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
